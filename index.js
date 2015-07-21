a2e5fae20c39de1e6877b4186dc3dbfdbfb762cf
James Talmage <james@talmage.io>
2015-07-21 16:58:19 -0400

    use through2 constructor mode.
    it reduces the number of parameters that need to be passed to the
    helper functions

--- index.js
+++ index.js
@@ -6,29 +6,35 @@ var EscapeCode = require('escape-code');
 
 module.exports = function(maxBuffer) {
   maxBuffer = typeof maxBuffer === 'number' ? maxBuffer : 25;
-  var decoder = null;
-  var prev = '';
-  return through2.obj(
-    function(chunk, enc, cb) {
-      if (!decoder) {
-        decoder = new StringDecoder(enc);
-      }
-      prev = run(prev + decoder.write(chunk), this, maxBuffer, true);
-      cb();
-    },
-    function(cb) {
-      if (prev) {
-        prev = run(prev, this, maxBuffer);
-        if (prev) {
-          this.push(prev);
-        }
-      }
-      cb();
-    }
-  );
+  var as = new AnsiStream();
+  as._maxBuffer = maxBuffer;
+  as._prev = '';
+  return as;
 };
 
-function run(str, obj, maxBuffer, moreAvailable) {
+var AnsiStream = through2.ctor(
+  {objectMode:true},
+  function handleChunk(chunk, enc, cb) {
+    var decoder = this._stringDecoder ||
+      (this._stringDecoder = new StringDecoder(enc));
+    this._prev = this._consumeString(this._prev + decoder.write(chunk), true);
+    cb();
+  },
+  function handleEndOfStream(cb) {
+    var prev = this._prev;
+    if (prev) {
+      prev = this._consumeString(prev);
+      if (prev) {
+        this.push(prev);
+      }
+    }
+    cb();
+  }
+);
+
+var asp = AnsiStream.prototype;
+
+asp._consumeString = function consumeString(str, moreAvailable) {
   var r = regex();
   var lastIndex = 0;
   var match;
@@ -36,7 +42,7 @@ function run(str, obj, maxBuffer, moreAvailable) {
   while ((match = r.exec(str))) {
     var nextIndex = match.index;
     if (nextIndex !== lastIndex) {
-      obj.push(str.substring(lastIndex, nextIndex));
+      this.push(str.substring(lastIndex, nextIndex));
     }
     var m0 = match[0];
     nextIndex += m0.length;
@@ -45,45 +51,40 @@ function run(str, obj, maxBuffer, moreAvailable) {
       return m0;
     }
 
-    obj.push(new EscapeCode(m0));
+    this.push(new EscapeCode(m0));
 
     lastIndex = nextIndex;
   }
 
-  return finalizeRemainder(str.substr(lastIndex), obj,  maxBuffer);
-}
+  return this._finalizeRemainder(str.substr(lastIndex));
+};
 
 /**
  * A hack to deal with the situation where an escape code sequence spans two chunks (very unlikely).
  * If it sees a potential start of the escape code sequence near the end of the chunk, it will
  * wait to see more characters before pushing.
  *
- * @param string - the end of the current chunk
- *
- * @param obj - the stream where we can push more text if we have exceeded the buffer size.
- *
- * @param maxBuffer - the maximum number of characters before we stop waiting for a match near the end
- *                    of the string and just push the text.
+ * @param string - the remaining end of the current chunk of the current chunk
  *
  * @returns {String} - The remaining text to leave in the buffer.
  */
-function finalizeRemainder(string, obj, maxBuffer) {
-  if (!maxBuffer) {
-    obj.push(string);
+asp._finalizeRemainder = function finalizeRemainder(string) {
+  if (!this._maxBuffer) {
+    this.push(string);
     return '';
   }
   if (string) {
-    var start = Math.max(0, string.length - maxBuffer);
+    var start = Math.max(0, string.length - this._maxBuffer);
     var p1 = string.indexOf('\u001b', start);
     var p2 = string.indexOf('\u009b', start);
     var p = Math.max(p1, p2);
     if (p < 0) {
-      obj.push(string);
+      this.push(string);
       string = '';
     } else if (p > 0) {
-      obj.push(string.substring(0, p));
+      this.push(string.substring(0, p));
       string = string.substring(p);
     }
   }
   return string;
-}
+};
